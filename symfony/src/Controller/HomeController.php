832d9ab28ca7870217c323c90ab2d6ca6a7f8410
JamalRonin <jamalelk60@gmail.com>
2022-11-26 14:48:08 +0100

    les routes et les pages html sont ok

--- symfony/src/Controller/HomeController.php
+++ symfony/src/Controller/HomeController.php
@@ -19,11 +19,11 @@ class HomeController extends AbstractController
     }
 
     /**
-     * @Route("/about", name="app_about")
+     * @Route("/apropos", name="app_about")
      */
     public function about(): Response
     {
-        return $this->render('about/index.html.twig', [
+        return $this->render('apropos/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
